a9100aa7b68b24f4c8cb96dc2446ba26bde2560e
Greg Mall <gregmall157@gmail.com>
2020-08-11 14:51:49 -0700

    location

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
+const { request } = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static('public'));
+const {
+  GEO_CODE,
+  WEATHER_CODE,
+  YELP_CODE,
+  HIKING_CODE
 
-function getLatLong(cityName){
-  const city = geoData[0];
+} = process.env;
+
+async function getLatLong(cityName){
+ const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEO_CODE}&q=${cityName}&format=json`);
+
+ const city = response.body[0];
+
     return {
       formatted_query: city.display_name,
       latitude: city.lat,
       longitude: city.lon,
     };
 }
-function getWeather(lat, lon) {
-  const data = weatherData.data;
+async function getWeather(lat, lon) {
+  const data = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key=${WEATHER_CODE}`)
   const forecastArray = data.map((weatherItem) =>{
     return {
         forecast: weatherItem.weather.description,
